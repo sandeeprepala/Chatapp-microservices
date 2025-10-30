0f21711c37ca2aab4617d62bd55a269fbec9aa96
sandeeprepala <sandeeprepala3@gmail.com>
2025-10-31 01:38:11 +0530

    changed user name

--- backend/mail/src/consumer.ts
+++ backend/mail/src/consumer.ts
@@ -7,7 +7,7 @@ const transporter = nodemailer.createTransport({
   host: "smtp.sendgrid.net",
   port: 587, // or 2525
   auth: {
-    user: "Connect",
+    user: "apikey",
     pass: process.env.SENDGRID_API_KEY,
   },
 });
